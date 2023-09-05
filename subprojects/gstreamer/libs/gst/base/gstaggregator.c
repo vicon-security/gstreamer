vicon-security/gstreamer

commit 6c66ede6db9952d196da0eba91146ad5fe0235fe

aggregator: fix start-time-selection=first on negative rate
When the property "start-time-selection" is set to "first", it
calculates the start time of the output from the buffer pts
(converting it to running time of the segment), but if the
rate is negative, the real start is not the pts, but the
pts + duration, because it plays from the end of the buffer
to it's start.
As a result of this bug, in the negative rate, when the
start-time-selection=first, the first frame is dropped
by the videoaggregator (reproduced on d3d11compositor).

subprojects/gstreamer/libs/gst/base/gstaggregator.c
@@ -3190,6 +3190,10 @@ gst_aggregator_pad_chain_internal (GstAggregator * self,
         if (aggpad->priv->head_segment.format == GST_FORMAT_TIME) {
           start_time = buf_pts;
           if (start_time != -1) {
+            GstClockTime buf_duration = GST_BUFFER_DURATION (buffer);
+            if (aggpad->priv->head_segment.rate < 0.0 && buf_duration != -1) {
+              start_time += buf_duration;
+            }
             start_time = MAX (start_time, aggpad->priv->head_segment.start);
             start_time =
                 gst_segment_to_running_time (&aggpad->priv->head_segment,

commit 78c97331fe2365960c4d1b2f6d0338b1f0efe0e9

fix for crash caused by commit 6c66ede6db9952d196da0eba91146ad5fe0235fe

subprojects/gstreamer/libs/gst/base/gstaggregator.c
@@ -3122,6 +3122,7 @@ gst_aggregator_pad_chain_internal (GstAggregator * self,
 {
   GstFlowReturn flow_return;
   GstClockTime buf_pts;
+  GstClockTime buf_duration;
 
   GST_TRACE_OBJECT (aggpad,
       "entering chain internal with %" GST_PTR_FORMAT, buffer);
@@ -3134,7 +3135,7 @@ gst_aggregator_pad_chain_internal (GstAggregator * self,
   PAD_UNLOCK (aggpad);
 
   buf_pts = GST_BUFFER_PTS (buffer);
-
+  buf_duration = GST_BUFFER_DURATION (buffer);
   for (;;) {
     SRC_LOCK (self);
     GST_OBJECT_LOCK (self);
@@ -3190,7 +3191,7 @@ gst_aggregator_pad_chain_internal (GstAggregator * self,
         if (aggpad->priv->head_segment.format == GST_FORMAT_TIME) {
           start_time = buf_pts;
           if (start_time != -1) {
-            GstClockTime buf_duration = GST_BUFFER_DURATION (buffer);
+            //GstClockTime buf_duration = GST_BUFFER_DURATION (buffer);
             if (aggpad->priv->head_segment.rate < 0.0 && buf_duration != -1) {
               start_time += buf_duration;
             }