0e38d2fcf1a6ce0c1313db51c01ad17fe2900649
Alexander Slobodeniuk <aslobodeniuk@fluendo.com>
2024-01-24 20:58:59 +0100

    d3d11upload: protect D3D11Device with device lock

--- subprojects/gst-plugins-bad/sys/d3d11/gstd3d11upload.cpp
+++ subprojects/gst-plugins-bad/sys/d3d11/gstd3d11upload.cpp
@@ -334,7 +334,11 @@ gst_d3d11_upload_decide_allocation (GstBaseTransform * trans, GstQuery * query)
   }
 
   device_handle = gst_d3d11_device_get_device_handle (filter->device);
+
+  gst_d3d11_device_lock (filter->device);
   hr = device_handle->CheckFormatSupport (dxgi_format, &supported);
+  gst_d3d11_device_unlock (filter->device);
+
   if (gst_d3d11_result (hr, filter->device)) {
     if ((supported & D3D11_FORMAT_SUPPORT_SHADER_SAMPLE) ==
         D3D11_FORMAT_SUPPORT_SHADER_SAMPLE) {
